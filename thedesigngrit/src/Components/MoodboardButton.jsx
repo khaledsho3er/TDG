b93e6cc4d869e21b06d12f72c655214025313dfd
khaledsho3er <khaled2002775@miuegypt.edu.eg>
2024-11-24 16:52:48 +0200

    header design fixing

---
+++ thedesigngrit/src/Components/MoodboardButton.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Box, IconButton } from "@mui/material";
+import MessageIcon from "@mui/icons-material/Message"; // You can replace this icon as needed.
+import { FaPaintBrush } from "react-icons/fa";
+
+function StickedButton() {
+  return (
+    <Box
+      sx={{
+        position: "absolute", // Absolute position relative to the parent container
+        top: 80, // Align to the top
+        left: 30, // Align to the left
+        zIndex: 1000,
+      }}
+    >
+      <IconButton
+        size="medium"
+        sx={{
+          backgroundColor: "#5F7C5F", // Green color
+          color: "#ffffff", // White icon color
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)", // Adds a subtle shadow
+          ":hover": {
+            backgroundColor: "#4E644E", // Darker green on hover
+          },
+        }}
+      >
+        <FaPaintBrush />
+      </IconButton>
+    </Box>
+  );
+}
+
+export default StickedButton;
