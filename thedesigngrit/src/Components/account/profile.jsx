f5d0886e35a4b7162183a024d7092057403f8cd8
Karim Ahmed Wahba <93930431+kkarimwahba@users.noreply.github.com>
2025-02-13 13:10:19 +0200

    Fix API endpoint in Profile component to use user ID from userData object

--- thedesigngrit/src/Components/account/profile.jsx
+++ thedesigngrit/src/Components/account/profile.jsx
@@ -47,7 +47,7 @@ function Profile({ userData }) {
     if (dialogAction === "save") {
       try {
         const response = await axios.put(
-          `https://tdg-db.onrender.com/api/updateUser/${userData.id}`,
+          `https://tdg-db.onrender.com/api/updateUser/${userData._id}`,
           formData, // Use the updated formData here
           { withCredentials: true }
         );
