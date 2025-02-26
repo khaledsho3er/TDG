b28ed5d94cc4509fc24cb4ac2d934c52e2f5dcb8
khaledsho3er <97366301+khaledsho3er@users.noreply.github.com>
2025-02-26 15:25:31 +0200

    Refactor Sustainability section to use MUI components for improved styling and structure

--- thedesigngrit/src/Components/home/Sustainability.jsx
+++ thedesigngrit/src/Components/home/Sustainability.jsx
@@ -1,36 +1,83 @@
 import React from "react";
-
+import {
+  Box,
+  Typography,
+  Card,
+  CardContent,
+  CardActions,
+  CardMedia,
+} from "@mui/material";
 const SustainabilitySection = () => {
   return (
-    <section className="sustainability-section">
-      {/* Image Container */}
-      <div className="sustainability-image-container">
-        <img
-          src="Assets/susSection.webp"
-          alt="Books and coffee cup on wooden table next to grey chair"
-          className="sustainability-image"
-        />
-      </div>
+    <Box className="build-package-section">
+      <Card
+        sx={{
+          background: "#6c7c59",
+          display: "flex",
+          justifyContent: "center",
+          borderRadius: "1.25rem",
+          boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+          height: "40rem",
+        }}
+        className="package-card"
+      >
+        <CardContent
+          sx={{
+            color: "#eae3e4",
+            padding: "3rem",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            flex: 1,
+            alignItems: "flex-start",
+          }}
+        >
+          <Typography
+            variant="h3"
+            fontSize="3.5rem"
+            fontWeight="600"
+            fontFamily={"Horizon"}
+            sx={{ paddingTop: "2rem" }}
+          >
+            What is TDG?
+          </Typography>
 
-      {/* Content Container */}
-      <div className="sustainability-content-container">
-        <div className="sustainability-content-wrapper">
-          {/* <span className="sustainability-label">Sustainability</span> */}
-
-          <h2 className="sustainability-heading">What is TDG ?</h2>
-
-          <p className="sustainability-description">
+          <Typography
+            variant="body1"
+            fontFamily={"Montserrat"}
+            fontSize={"1rem"}
+            sx={{ marginTop: "-2.5rem" }}
+          >
             TheDesignGrit is here to spotlight Egyptian design. We’re giving
             local brands the platform they deserve, connecting their
             craftsmanship with those who value it most. It’s about honoring
             tradition, celebrating innovation, and helping you discover pieces
             that make you feel most at home.
-          </p>
+          </Typography>
 
-          <button className="sustainability-button">Learn More</button>
-        </div>
-      </div>
-    </section>
+          <CardActions>
+            <button
+              className="btn"
+              style={{
+                color: "black",
+                background: "#eae3e4",
+                fontWeight: "bold",
+              }}
+            >
+              Find more
+            </button>
+          </CardActions>
+        </CardContent>
+
+        <CardMedia sx={{ flex: 1 }}>
+          <img
+            src="Assets/susSection.webp"
+            alt="Green Bowl Beach, Bali, Indonesia_2AGYRXP 1"
+            style={{ width: "100%", height: "100%" }}
+          />
+        </CardMedia>
+      </Card>
+    </Box>
   );
 };
 
