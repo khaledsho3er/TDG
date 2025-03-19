fcada4bb8135d9e39a4ec62a8ff16ea6bdb4f084
Karim Ahmed Wahba <93930431+kkarimwahba@users.noreply.github.com>
2025-03-19 17:38:22 +0200

    Update OrderDetails component to display sub-order status instead of order status

--- thedesigngrit/src/Components/vendorSide/orderDetails.jsx
+++ thedesigngrit/src/Components/vendorSide/orderDetails.jsx
@@ -672,7 +672,7 @@ const OrderDetails = ({ order, onBack }) => {
                           minWidth: "80px",
                         }}
                       >
-                        {order.orderStatus}
+                        {prodcut.subOrderStatus}
                       </span>
                     </td>
                     <td>{product.totalPrice} LE</td>
