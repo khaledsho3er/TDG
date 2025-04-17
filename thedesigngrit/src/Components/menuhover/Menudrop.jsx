5af5a42dca721fa302ec52e180a3490dd4597548
khaledsho3er <97366301+khaledsho3er@users.noreply.github.com>
2025-04-17 15:45:05 +0200

    Add a wrapper div in Menudrop component for improved layout structure

--- thedesigngrit/src/Components/menuhover/Menudrop.jsx
+++ thedesigngrit/src/Components/menuhover/Menudrop.jsx
@@ -83,6 +83,8 @@ const Menudrop = ({ category, onMouseEnter, onMouseLeave }) => {
             </>
           ) : null}
         </div>
+      </div>
+      <div>
         <div style={{ width: "100%", height: "100%", objectFit: "cover" }}>
           {selectedDetail && (
             <img
