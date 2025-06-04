1406b66e8b40b78d0ee9acb2363ba6dcee23858b
khaledsho3er <karimwahba53@gmail.com>
2025-06-04 19:38:59 +0300

    Refactor PaymentForm: restructure payment method rendering to always display the form when iframeUrl is not set, improving user experience and ensuring consistent access to payment options.

--- thedesigngrit/src/Components/Checkout/Paymentmethod.jsx
+++ thedesigngrit/src/Components/Checkout/Paymentmethod.jsx
@@ -140,159 +140,8 @@ function PaymentForm({
   //
   return (
     <Box className="paymentmethod-container">
-      <Box className="paymentmethod-firstrow-firstcolumn">
-        <FormControl fullWidth>
-          <RadioGroup
-            value={paymentMethod}
-            onChange={handlePaymentMethodChange}
-          >
-            {/* Card Payment Option */}
-            <Box className="paymentmethod-radio-dropdown">
-              <FormControlLabel
-                value="card"
-                control={<Radio />}
-                label={<span className="horizon-font">Card</span>}
-              />
-              {paymentMethod === "card" && (
-                <FormControl className="paymentmethod-dropdown">
-                  <Select
-                    value={selectedCard}
-                    onChange={(e) => setSelectedCard(e.target.value)}
-                    displayEmpty
-                    className="montserrat-font"
-                  >
-                    <MenuItem value="" disabled>
-                      Card Type
-                    </MenuItem>
-                    {cardOptions.map((card, index) => (
-                      <MenuItem key={index} value={card.type}>
-                        {card.type}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-              )}
-            </Box>
-
-            {paymentMethod === "card" && (
-              <Box className="paymentmethod-card-details">
-                {paymentError && (
-                  <Alert severity="error" sx={{ mb: 2 }}>
-                    {paymentError}
-                  </Alert>
-                )}
-                <TextField
-                  fullWidth
-                  margin="normal"
-                  label="Card Number"
-                  name="cardNumber"
-                  InputLabelProps={{ className: "montserrat-font" }}
-                  className="montserrat-font"
-                  value={cardDetails.cardNumber}
-                  onChange={handleCardDetailsChange}
-                  error={!!errors.cardNumber}
-                  helperText={errors.cardNumber}
-                />
-                <Box display="flex" gap={2}>
-                  <TextField
-                    fullWidth
-                    margin="normal"
-                    label="MM/YY"
-                    name="expiry"
-                    InputLabelProps={{ className: "montserrat-font" }}
-                    className="montserrat-font"
-                    value={cardDetails.expiry}
-                    onChange={handleCardDetailsChange}
-                    error={!!errors.expiry}
-                    helperText={errors.expiry}
-                  />
-                  <TextField
-                    fullWidth
-                    margin="normal"
-                    label="CVV"
-                    name="cvv"
-                    type="password"
-                    InputLabelProps={{ className: "montserrat-font" }}
-                    className="montserrat-font"
-                    value={cardDetails.cvv}
-                    onChange={handleCardDetailsChange}
-                    error={!!errors.cvv}
-                    helperText={errors.cvv}
-                  />
-                </Box>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  className="montserrat-font"
-                  onClick={handlePayNow}
-                  disabled={isProcessing}
-                  sx={{ mt: 2 }}
-                >
-                  {isProcessing ? (
-                    <CircularProgress size={24} color="inherit" />
-                  ) : (
-                    "Pay Now"
-                  )}
-                </Button>
-              </Box>
-            )}
-
-            {/* Other Payment Options */}
-            <FormControlLabel
-              sx={{ borderBottom: "1px solid black" }}
-              value="valu"
-              control={<Radio />}
-              label={
-                <span className="montserrat-font">Valu, Halan, Saholha</span>
-              }
-            />
-            <FormControlLabel
-              sx={{ borderBottom: "1px solid black" }}
-              value="fawry"
-              control={<Radio />}
-              label={<span className="montserrat-font">Fawry Pay</span>}
-            />
-            {showCOD ? (
-              <FormControlLabel
-                value="cod"
-                control={<Radio />}
-                label={
-                  <span className="montserrat-font">Cash on Delivery</span>
-                }
-              />
-            ) : (
-              <Box
-                sx={{
-                  padding: "8px 0",
-                  color: "#888",
-                  fontStyle: "italic",
-                  display: "flex",
-                  alignItems: "center",
-                  gap: "8px",
-                }}
-                className="montserrat-font"
-              >
-                <span
-                  style={{
-                    width: "24px",
-                    height: "24px",
-                    display: "inline-block",
-                  }}
-                ></span>
-                Cash on Delivery unavailable for orders over 10,000 EGP
-              </Box>
-            )}
-          </RadioGroup>
-        </FormControl>
-      </Box>
-      <BillSummary
-        cartItems={billData.cartItems}
-        subtotal={billData.subtotal}
-        shippingFee={billData.shippingFee}
-        total={billData.total}
-      />
-      {/* Render the Paymob iframe if iframeUrl is set */}
-      {iframeUrl && (
+      {/* Always render the iframe if iframeUrl is set */}
+      {iframeUrl ? (
         <iframe
           src={iframeUrl}
           style={{
@@ -304,7 +153,165 @@ function PaymentForm({
           allow="camera *; microphone *"
           title="Paymob Payment"
         />
+      ) : (
+        // Show the payment form if iframeUrl is not set
+        <Box>
+          <Box className="paymentmethod-firstrow-firstcolumn">
+            <FormControl fullWidth>
+              <RadioGroup
+                value={paymentMethod}
+                onChange={handlePaymentMethodChange}
+              >
+                {/* Card Payment Option */}
+                <Box className="paymentmethod-radio-dropdown">
+                  <FormControlLabel
+                    value="card"
+                    control={<Radio />}
+                    label={<span className="horizon-font">Card</span>}
+                  />
+                  {paymentMethod === "card" && (
+                    <FormControl className="paymentmethod-dropdown">
+                      <Select
+                        value={selectedCard}
+                        onChange={(e) => setSelectedCard(e.target.value)}
+                        displayEmpty
+                        className="montserrat-font"
+                      >
+                        <MenuItem value="" disabled>
+                          Card Type
+                        </MenuItem>
+                        {cardOptions.map((card, index) => (
+                          <MenuItem key={index} value={card.type}>
+                            {card.type}
+                          </MenuItem>
+                        ))}
+                      </Select>
+                    </FormControl>
+                  )}
+                </Box>
+
+                {paymentMethod === "card" && (
+                  <Box className="paymentmethod-card-details">
+                    {paymentError && (
+                      <Alert severity="error" sx={{ mb: 2 }}>
+                        {paymentError}
+                      </Alert>
+                    )}
+                    <TextField
+                      fullWidth
+                      margin="normal"
+                      label="Card Number"
+                      name="cardNumber"
+                      InputLabelProps={{ className: "montserrat-font" }}
+                      className="montserrat-font"
+                      value={cardDetails.cardNumber}
+                      onChange={handleCardDetailsChange}
+                      error={!!errors.cardNumber}
+                      helperText={errors.cardNumber}
+                    />
+                    <Box display="flex" gap={2}>
+                      <TextField
+                        fullWidth
+                        margin="normal"
+                        label="MM/YY"
+                        name="expiry"
+                        InputLabelProps={{ className: "montserrat-font" }}
+                        className="montserrat-font"
+                        value={cardDetails.expiry}
+                        onChange={handleCardDetailsChange}
+                        error={!!errors.expiry}
+                        helperText={errors.expiry}
+                      />
+                      <TextField
+                        fullWidth
+                        margin="normal"
+                        label="CVV"
+                        name="cvv"
+                        type="password"
+                        InputLabelProps={{ className: "montserrat-font" }}
+                        className="montserrat-font"
+                        value={cardDetails.cvv}
+                        onChange={handleCardDetailsChange}
+                        error={!!errors.cvv}
+                        helperText={errors.cvv}
+                      />
+                    </Box>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      className="montserrat-font"
+                      onClick={handlePayNow}
+                      disabled={isProcessing}
+                      sx={{ mt: 2 }}
+                    >
+                      {isProcessing ? (
+                        <CircularProgress size={24} color="inherit" />
+                      ) : (
+                        "Pay Now"
+                      )}
+                    </Button>
+                  </Box>
+                )}
+
+                {/* Other Payment Options */}
+                <FormControlLabel
+                  sx={{ borderBottom: "1px solid black" }}
+                  value="valu"
+                  control={<Radio />}
+                  label={
+                    <span className="montserrat-font">
+                      Valu, Halan, Saholha
+                    </span>
+                  }
+                />
+                <FormControlLabel
+                  sx={{ borderBottom: "1px solid black" }}
+                  value="fawry"
+                  control={<Radio />}
+                  label={<span className="montserrat-font">Fawry Pay</span>}
+                />
+                {showCOD ? (
+                  <FormControlLabel
+                    value="cod"
+                    control={<Radio />}
+                    label={
+                      <span className="montserrat-font">Cash on Delivery</span>
+                    }
+                  />
+                ) : (
+                  <Box
+                    sx={{
+                      padding: "8px 0",
+                      color: "#888",
+                      fontStyle: "italic",
+                      display: "flex",
+                      alignItems: "center",
+                      gap: "8px",
+                    }}
+                    className="montserrat-font"
+                  >
+                    <span
+                      style={{
+                        width: "24px",
+                        height: "24px",
+                        display: "inline-block",
+                      }}
+                    ></span>
+                    Cash on Delivery unavailable for orders over 10,000 EGP
+                  </Box>
+                )}
+              </RadioGroup>
+            </FormControl>
+          </Box>
+          <Button onClick={handlePayNow}>Pay Now</Button>
+        </Box>
       )}
+      <BillSummary
+        cartItems={billData.cartItems}
+        subtotal={billData.subtotal}
+        shippingFee={billData.shippingFee}
+        total={billData.total}
+      />
     </Box>
   );
 }
