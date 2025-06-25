40f36e07e033664ee9a656321e235d448ecee266
khaledsho3er <karimwahba53@gmail.com>
2025-06-25 18:46:20 +0300

    Refactor TrackQuotation component to simplify payment button rendering by removing redundant conditions for 'Paid' status display

--- thedesigngrit/src/Components/TrackQuotation.jsx
+++ thedesigngrit/src/Components/TrackQuotation.jsx
@@ -264,15 +264,7 @@ function TrackQuotation() {
           >
             {selectedQuotation?.ClientApproval === true &&
               selectedQuotation?.vendorApproval === true &&
-              selectedQuotation?.quotePrice &&
-              (selectedQuotation?.paid === true ||
-              selectedQuotation?.paymentStatus === "Paid" ? (
-                <Typography
-                  sx={{ fontWeight: "bold", color: "#6b7b58", mr: 2 }}
-                >
-                  Paid
-                </Typography>
-              ) : (
+              selectedQuotation?.quotePrice && (
                 <button
                   onClick={handlePayNow}
                   className="submit-btn"
@@ -281,7 +273,7 @@ function TrackQuotation() {
                 >
                   {payLoading ? "Processing..." : "Pay Now"}
                 </button>
-              ))}
+              )}
             {selectedQuotation?.status === "approved" ? (
               <Typography sx={{ fontWeight: "bold" }}>Deal Sealed</Typography>
             ) : selectedQuotation?.status === "rejected" ? (
